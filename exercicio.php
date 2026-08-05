b6088b4f122a71f5f968135439dc104577e7ad5a
anaduraes10 <anaclaraduraesdesouza0@gmail.com>
2026-08-05 16:43:49 -0300

    Add PHP script to select a day of the week

---
+++ exercicio.php
@@ -0,0 +1,42 @@
+<?php
+$dia;
+
+$dia = readline ("EScolha um dia da semana:");
+
+switch($dia){
+    case 1;
+    print("Você escolheu domingo");
+    break;
+    
+    case 2; 
+    print("Você escolheu segunda ");
+    break;
+    
+    case 3;
+    print("Você escolheu terça ");
+    break;
+    
+    case 4;
+    print("Você escolheu quarta");
+    break;
+    
+    case 5;
+    print("Você escolheu quinta");
+    break;
+    
+    case 6;
+    print("Você escolheu sexta");
+    break;
+    
+    case 7;
+    print("Você escolheu sabádo");
+    break; 
+    
+    default:
+        print "Operação invalida";
+        break;
+    
+    
+    
+}
+?>
