bf2181a2d008028b2944ec0a5e51555f6868fc67
anaduraes10 <anaclaraduraesdesouza0@gmail.com>
2026-08-05 15:17:19 -0300

    Add basic arithmetic operations in exercicio1.php

---
+++ exercicio1.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Digite o primeiro número: ";
+$nu1 = (float) readline();
+
+echo "Digite o segundo número: ";
+$nu2 = (float) readline(); 
+
+$soma = $nu1 + $nu2;
+$subitração = $nu1 - $nu2;
+$mutiplicação = $nu1 * $nu2;
+
+echo "A soma é $soma";
+
+echo "A subitração é $subitração ";
+echo "A mutiplicação é $mutiplicação ";
+?>
